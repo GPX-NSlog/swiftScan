2524ad9dfbe7bbb73f21d70cac1ac3ff6a0bf4ee
Gpx <GpxNSLog@gmail.com>
2022-03-25 18:44:41 +0800

    修复相册选择“非二维码”图片不走回调的问题

--- Source/LBXScanViewController.swift
+++ Source/LBXScanViewController.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2015年 xialibing. All rights reserved.
 //
 
-import UIKit
-import Foundation
 import AVFoundation
+import Foundation
+import UIKit
 
 public protocol LBXScanViewControllerDelegate: class {
-     func scanFinished(scanResult: LBXScanResult, error: String?)
+    func scanFinished(scanResult: LBXScanResult, error: String?)
 }
 
 public protocol QRRectDelegate {
@@ -19,7 +19,6 @@ public protocol QRRectDelegate {
 }
 
 open class LBXScanViewController: UIViewController {
-    
     // 返回扫码结果，也可以通过继承本控制器，改写该handleCodeResult方法即可
     open weak var scanResultDelegate: LBXScanViewControllerDelegate?
 
@@ -33,9 +32,9 @@ open class LBXScanViewController: UIViewController {
 
     // 启动区域识别功能
     open var isOpenInterestRect = false
-    
-    //连续扫码
-    open var isSupportContinuous = false;
+
+    // 连续扫码
+    open var isSupportContinuous = false
 
     // 识别码的类型
     public var arrayCodeType: [AVMetadataObject.ObjectType]?
@@ -46,7 +45,7 @@ open class LBXScanViewController: UIViewController {
     // 相机启动提示文字
     public var readyString: String! = "loading"
 
-    open override func viewDidLoad() {
+    override open func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -65,7 +64,7 @@ open class LBXScanViewController: UIViewController {
         isOpenInterestRect = isOpen
     }
 
-    open override func viewDidAppear(_ animated: Bool) {
+    override open func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         drawScanView()
         perform(#selector(LBXScanViewController.startScan), with: nil, afterDelay: 0.3)
@@ -89,19 +88,19 @@ open class LBXScanViewController: UIViewController {
                                      objType: arrayCodeType!,
                                      isCaptureImg: isNeedCodeImage,
                                      cropRect: cropRect,
-                                     success: { [weak self] (arrayResult) -> Void in
-                                        guard let strongSelf = self else {
-                                            return
-                                        }
-                                        if !strongSelf.isSupportContinuous {
-                                            // 停止扫描动画
-                                            strongSelf.qRScanView?.stopScanAnimation()
-                                        }
-                                        strongSelf.handleCodeResult(arrayResult: arrayResult)
+                                     success: { [weak self] arrayResult in
+                                         guard let strongSelf = self else {
+                                             return
+                                         }
+                                         if !strongSelf.isSupportContinuous {
+                                             // 停止扫描动画
+                                             strongSelf.qRScanView?.stopScanAnimation()
+                                         }
+                                         strongSelf.handleCodeResult(arrayResult: arrayResult)
                                      })
         }
-        
-        scanObj?.supportContinuous = isSupportContinuous;
+
+        scanObj?.supportContinuous = isSupportContinuous
 
         // 结束相机等待提示
         qRScanView?.deviceStopReadying()
@@ -112,7 +111,7 @@ open class LBXScanViewController: UIViewController {
         // 相机运行
         scanObj?.start()
     }
-    
+
     open func drawScanView() {
         if qRScanView == nil {
             qRScanView = LBXScanView(frame: view.frame, vstyle: scanStyle!)
@@ -121,7 +120,6 @@ open class LBXScanViewController: UIViewController {
         }
         qRScanView?.deviceStartReadying(readyStr: readyString)
     }
-   
 
     /**
      处理扫码结果，如果是继承本控制器的，可以重写该方法,作出相应地处理，或者设置delegate作出相应处理
@@ -130,12 +128,11 @@ open class LBXScanViewController: UIViewController {
         guard let delegate = scanResultDelegate else {
             fatalError("you must set scanResultDelegate or override this method without super keyword")
         }
-        
+
         if !isSupportContinuous {
             navigationController?.popViewController(animated: true)
-
         }
-        
+
         if let result = arrayResult.first {
             delegate.scanFinished(scanResult: result, error: nil)
         } else {
@@ -143,13 +140,13 @@ open class LBXScanViewController: UIViewController {
             delegate.scanFinished(scanResult: result, error: "no scan result")
         }
     }
-    
-    open override func viewWillDisappear(_ animated: Bool) {
+
+    override open func viewWillDisappear(_ animated: Bool) {
         NSObject.cancelPreviousPerformRequests(withTarget: self)
         qRScanView?.stopScanAnimation()
         scanObj?.stop()
     }
-    
+
     @objc open func openPhotoAlbum() {
         LBXPermissions.authorizePhotoWith { [weak self] _ in
             let picker = UIImagePickerController()
@@ -161,13 +158,14 @@ open class LBXScanViewController: UIViewController {
     }
 }
 
-//MARK: - 图片选择代理方法
+// MARK: - 图片选择代理方法
+
 extension LBXScanViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    //MARK: -----相册选择图片识别二维码 （条形码没有找到系统方法）
+    // MARK: - ----相册选择图片识别二维码 （条形码没有找到系统方法）
+
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         picker.dismiss(animated: true, completion: nil)
-        
+
         let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
         let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         guard let image = editedImage ?? originalImage else {
@@ -175,21 +173,19 @@ extension LBXScanViewController: UIImagePickerControllerDelegate, UINavigationCo
             return
         }
         let arrayResult = LBXScanWrapper.recognizeQRImage(image: image)
-        if !arrayResult.isEmpty {
+//        if !arrayResult.isEmpty {
             handleCodeResult(arrayResult: arrayResult)
-        }
+//        }
     }
-    
 }
 
-//MARK: - 私有方法
+// MARK: - 私有方法
+
 private extension LBXScanViewController {
-    
     func showMsg(title: String?, message: String?) {
         let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let alertAction = UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default, handler: nil)
         alertController.addAction(alertAction)
         present(alertController, animated: true, completion: nil)
     }
-    
 }
